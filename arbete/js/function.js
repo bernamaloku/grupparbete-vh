bec9a50a052adb2bb65508303f45b8c2bd001512
Belula Belai <belulaaab@hotmail.com>
2023-01-18 21:11:19 +0100

    Update function.js

---
+++ arbete/js/function.js
@@ -0,0 +1,19 @@
+let weather = {
+    apiKey: "2e27da28ee464709326343fb9e56ad73",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    }
+};
