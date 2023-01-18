60d292abe6fcd115d5bc8803160302830dbad563
Belula Belai <belulaaab@hotmail.com>
2023-01-18 22:18:28 +0100

    Update function.js

--- arbete/js/function.js
+++ arbete/js/function.js
@@ -15,5 +15,21 @@ let weather = {
                 return response.json();
             })
             .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
     }
 };
